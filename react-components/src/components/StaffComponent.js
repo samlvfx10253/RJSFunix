4e295f014b503442d55959226d87a7668f90d3b3
samlvfx10253 <samlvfx10253@funix.edu.vn>
2021-12-20 23:01:04 +0700

    update asm1

--- react-components/src/components/StaffComponent.js
+++ react-components/src/components/StaffComponent.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Card, CardTitle } from 'reactstrap';
+import StaffDetail from './StaffDetailComponent';
 
 class Staff extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            staffSelected: null
+            selectedStaff: null
         }
+    }
+    onSelectedStaff(staff) {
+        this.setState({ selectedStaff: staff })
     }
     render() {
         const listStaff = this.props.staffs.map((staff) => {
             return (
-                <div className='col-12 col-md-5 col-xl-3 m-1'>
-                    <Card key={staff.id}>
+                <div key={staff.id} className='col-12 col-md-5 col-xl-3 m-1'>
+                    <Card className='card-container' key={staff.id}
+                        style={{cursor: "pointer"}}
+                        onClick={() => this.onSelectedStaff(staff)}>
                         <CardTitle>{staff.name}</CardTitle>
                     </Card>
                 </div>
@@ -20,9 +26,11 @@ class Staff extends Component {
         });
         return (
             <div className='container'>
+                <h2>Danh sách nhân viên</h2>
                 <div className='row'>
                     {listStaff}
                 </div>
+                <StaffDetail staff={this.state.selectedStaff} />
             </div>
         );
     }
---
+++ react-components/src/components/StaffDetailComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
+
+class StaffDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        if (this.props.staff != null) {
+            return (
+                <div className="container">
+                    <Card>
+                        <h4>Thông tin chi tiết</h4>
+                        <div className="row">
+                            <div className="col-12 col-md-3">
+                                <CardImg top src={this.props.staff.image} alt={this.props.staff.name} />
+                            </div>
+                            <div className="col-12 col-md-9">
+                                <CardBody>
+                                    <CardTitle tag='h4'>{this.props.staff.name}</CardTitle>
+                                    <ul className="list-unstyled">
+                                        <li><p>Phòng ban: {this.props.staff.department.name}</p></li>
+                                        <li><p>Chức vụ: {this.props.staff.role}</p></li>
+                                        <li><p>Ngày sinh: {new Intl.DateTimeFormat('en-GB')
+                                            .format(new Date(this.props.staff.doB))}
+                                        </p></li>
+                                        <li><p>Ngày vào công ty: {new Intl.DateTimeFormat('en-GB')
+                                            .format(new Date(this.props.staff.startDate))}</p></li>
+                                        <li><p>Số ngày nghỉ còn lại: {this.props.staff.annualLeave}</p></li>
+                                        <li><p>Số ngày đã làm thêm: {this.props.staff.overTime}</p></li>
+                                    </ul>
+                                </CardBody>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            )
+        }
+        else {
+            return (<></>)
+        }
+    }
+}
+export default StaffDetail;
