51a73be7b83ce69af9badabcce81fa190b34d9ad
samlvfx10253 <samlvfx10253@ed.funix.vn>
2022-01-24 15:49:58 +0700

    Header and Footer

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+
 import Main from './components/MainComponents';
 
 class App extends Component {
