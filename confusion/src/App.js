9087a161269e56c160ddba5a2a09b314f1fc6604
samlvfx10253 <samlvfx10253@ed.funix.vn>
2022-01-25 10:53:26 +0700

    React Router

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,9 +11,11 @@ import Main from './components/MainComponents';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     )
   }
 }
--- confusion/src/components/MainComponents.js
+++ confusion/src/components/MainComponents.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -13,17 +15,21 @@ class Main extends Component {
             selectedDish: null
         };
     }
-    onSelectedDish(dishId) {
-        this.setState({ selectedDish: dishId })
-    }
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
+
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onSelectedDish(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) =>
-                    dish.id === this.state.selectedDish)[0]} />
+                <Routes>
+                    <Route path='/home' element={<HomePage />} />
+                    <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />} />
+                </Routes>
                 <Footer />
             </div>
         )
